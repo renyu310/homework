48061804cdddcefbbe66a3c4b530d5f367006fde
renyu310 <zhang.yuren@hotmail.com>
2015-10-28 14:47:51 +0800

    secong

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
   def Movie.ratingcollection
     ratingcollect=Array.new
     i=0
-    Movie.select(:Rating).distinct.each  do |movie|
+    Movie.select(:rating).distinct.each  do |movie|
       ratingcollect[i]=movie.rating
       i+=1;
     end
